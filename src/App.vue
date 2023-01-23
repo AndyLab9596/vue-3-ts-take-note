ed78dbfd0dbe3c79fac2da0f62ed2a0a15d5972a
andyvy <thienvy95@gmail.com>
2023-01-23 15:31:07 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <RouterLink to="/" class="button">Notes</RouterLink> |
-  <RouterLink to="/stats" class="button">Stats</RouterLink>
-
+  <NavBar />
   <RouterView />
 </template>
+
+<script lang="ts" setup>
+import NavBar from "@/components/Layout/NavBar.vue";
+</script>
 
 <style>
 @import "bulma/css/bulma.min.css";
